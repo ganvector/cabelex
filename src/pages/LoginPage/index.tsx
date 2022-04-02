6ebf1b9527a8b31b225461164f3872647218755e
Vitor Nascimento <vitor.gan21@gmail.com>
2022-04-02 14:25:08 -0300

    Modificado para funcionar com contexto

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,12 +1,16 @@
 import LoginForm from "./LoginForm";
 import AuthService from "../../services/auth.service";
+import { useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import ILoginCredentials from "../../interfaces/login-credentiais.interface";
 
 const LoginPage = () => {
-  const signin = async (username: string, password: string) => {
-    console.log(username, password);
+  const ctx = useContext(AuthContext);
+
+  const login = async (loginCredentials: ILoginCredentials) => {
     try {
-      await AuthService.signin(username, password);
-      window.location.hash = "/tasks";
+      let accessToken = await AuthService.login(loginCredentials);
+      ctx.loginHandler(accessToken);
     } catch (error: any) {
       alert(error.message);
     }
@@ -14,7 +18,7 @@ const LoginPage = () => {
 
   return (
     <div>
-      <LoginForm signin={signin} />
+      <LoginForm login={login} />
     </div>
   );
 };

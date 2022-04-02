31d4f31bc29ad36570c0151d818228af24d7c6d8
Vitor Nascimento <vitor.gan21@gmail.com>
2022-04-02 14:29:06 -0300

    Aplicando novas paginas e e marcacao de login atraves de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 import LoginPage from "./pages/LoginPage";
+import AuthContext from "./store/auth-context";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import MainHeader from "./components/MainHeader";
 
 function App() {
+  const [isLoggedIn, setIsLoggerIn] = useState(false);
+
+  const loginHandler = (accessToken: string) => {
+    localStorage.setItem("accessToken", accessToken);
+    setIsLoggerIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("accessToken");
+    setIsLoggerIn(false);
+  };
+
   return (
     <div className="App">
-      <div>Cabelex</div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthContext.Provider
+        value={{
+          isLogged: isLoggedIn,
+          loginHandler: loginHandler,
+          logoutHandler: logoutHandler,
+        }}
+      >
+        <MainHeader />
+        <main>
+          {!isLoggedIn && <LoginPage />}
+          {isLoggedIn && <Home />}
+        </main>
+      </AuthContext.Provider>
     </div>
   );
 }

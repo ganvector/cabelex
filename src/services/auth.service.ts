ec4eeed140a2fc2b3bb8f1f431bea62595ffba29
Vitor Nascimento <vitor.gan21@gmail.com>
2022-04-02 14:27:19 -0300

    mudado nome da funcao de signin para login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,8 @@
+import ILoginCredentials from "../interfaces/login-credentiais.interface";
+
 export default class AuthService {
-  static async signin(username: string, password: string) {
+  static async login(loginCredentials: ILoginCredentials) {
+    const { username, password } = loginCredentials;
     if (!username || !password || !username.length || !password.length) {
       throw new Error('"nome de usuário" ou "senha" não fornecidos');
     }
@@ -10,12 +13,10 @@ export default class AuthService {
     let user = users.find((obj) => obj.username === username);
 
     if (!user || user.password !== password) {
-      throw new Error('"nome de usuario" ou "senha"inválidos.');
+      throw new Error('"nome de usuario" ou "senha" inválidos.');
     }
 
-    this.saveToken(user.accessToken);
-    this.saveUser(user);
-    return username;
+    return user.accessToken;
   }
 
   static saveToken(accessToken: string) {
@@ -26,5 +27,18 @@ export default class AuthService {
     const { password, accessToken, ...userJSON } = user;
 
     localStorage.setItem("user", JSON.stringify(userJSON));
+  }
+
+  static async getUser() {
+    const accessToken = localStorage.getItem("accessToken");
+
+    // @ts-ignore
+    const user = document.users[0];
+
+    if (user.accessToken !== accessToken) {
+      throw new Error("401 (Unauthorized)");
+    }
+
+    return user;
   }
 }

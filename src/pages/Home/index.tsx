5ceb68451d74fa688520547255f945a4aa6341d0
Vitor Nascimento <vitor.gan21@gmail.com>
2022-04-02 21:47:48 -0300

    Retirado BrowserRoutes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import FiliaisPage from "../FiliaisPage";
 import FuncionariosPage from "../FuncionariosPage";
 
 const Home = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<FiliaisPage />} />
-          <Route path="/filiais" element={<FiliaisPage />} />
-          <Route path="/funcionarios" element={<FuncionariosPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Routes>
+      <Route path="/" element={<FiliaisPage />} />
+      <Route path="/filiais" element={<FiliaisPage />} />
+      <Route path="/funcionarios" element={<FuncionariosPage />} />
+    </Routes>
   );
 };
 

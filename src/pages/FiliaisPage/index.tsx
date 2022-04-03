0e420fe9d360c3df30ac2dde18a695a080881216
Vitor Nascimento <vitor.gan21@gmail.com>
2022-04-02 21:55:40 -0300

    Adicionado table de filiais

--- src/pages/FiliaisPage/index.tsx
+++ src/pages/FiliaisPage/index.tsx
@@ -1,9 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import FiliaisService from "../../services/filiais.service";
+import FiliaisList from "./FiliaisList";
+import AddFilialModal from "./AddFilialModal";
 
 const FiliaisPage = () => {
-  useEffect(() => {}, []);
+  const [filiaisList, setFiliaisList] = useState([]);
 
-  return <div>Filiais Page</div>;
+  useEffect(() => {
+    FiliaisService.getFiliais().then((filiais: any) => {
+      setFiliaisList(filiais);
+    });
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <AddFilialModal />
+      </div>
+      <div>
+        <FiliaisList filiais={filiaisList} />
+      </div>
+    </div>
+  );
 };
 
 export default FiliaisPage;

a24596815c9c47129eb35f63179c2a7a57a0a778
Vitor Nascimento <vitor.gan21@gmail.com>
2022-04-03 18:18:16 -0300

    Adicionado form para criacao e edicao do funcionario

---
+++ src/pages/FuncionariosPage/FuncionarioForm.tsx
@@ -0,0 +1,86 @@
+import Form from "react-bootstrap/Form";
+import { useContext, useState } from "react";
+import Button from "react-bootstrap/Button";
+import FuncionariosContext from "../../store/funcionarios-context";
+import FuncionariosService from "../../services/funcionarios.service";
+
+const FuncionarioForm = (props: any) => {
+  const [funcionarioFilialInput, setFuncionarioFilialInput] = useState(
+    props.funcionario?.filial_id || "0"
+  );
+  const [funcionarioNomeInput, setFuncionarioNomeInput] = useState(
+    props.funcionario?.nome || ""
+  );
+
+  const ctx = useContext(FuncionariosContext);
+
+  const handleFuncionarioNomeInputChange = (event: any) => {
+    setFuncionarioNomeInput(event.target.value);
+  };
+
+  const handleFuncionarioFilialInputChange = (event: any) => {
+    setFuncionarioFilialInput(event.target.value);
+  };
+
+  const filiaisOptions = ctx.filiais.map((filial: any) => (
+    <option value={filial._id}>{filial.nome}</option>
+  ));
+
+  const handleFormSubmit = () => {
+    if (!funcionarioNomeInput.trim().length) {
+      alert("Nome do funcionário deve ser preenchido");
+      return;
+    }
+
+    if (funcionarioFilialInput === "0") {
+      alert("Filial deve ser indicada");
+      return;
+    }
+
+    if (props.funcionario) {
+      FuncionariosService.update(props.funcionario._id, {
+        nome: funcionarioNomeInput,
+        filial_id: funcionarioFilialInput,
+      }).then(props.onHideModal);
+      ctx.refreshComponent();
+      return;
+    }
+
+    FuncionariosService.create({
+      nome: funcionarioNomeInput,
+      filial_id: funcionarioFilialInput,
+    }).then(props.onHideModal);
+    ctx.refreshComponent();
+  };
+
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Label>Nome</Form.Label>
+        <Form.Control
+          type="text"
+          value={funcionarioNomeInput}
+          onChange={handleFuncionarioNomeInputChange}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Filial</Form.Label>
+        <Form.Select
+          value={funcionarioFilialInput}
+          onChange={handleFuncionarioFilialInputChange}
+        >
+          <option value="0">Escolha uma filial</option>
+          {filiaisOptions}
+        </Form.Select>
+      </Form.Group>
+      <Button variant="primary" onClick={handleFormSubmit}>
+        Salvar
+      </Button>
+      <Button variant="danger" onClick={props.onHideModal}>
+        Cancelar
+      </Button>
+    </Form>
+  );
+};
+
+export default FuncionarioForm;

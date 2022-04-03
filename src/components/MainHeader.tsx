386f1cf5b32716c849e98915c0a3668d463dbea6
Vitor Nascimento <vitor.gan21@gmail.com>
2022-04-03 19:48:33 -0300

    Novo header

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -2,20 +2,35 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../store/auth-context";
 import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
 const MainHeader = () => {
   const ctx = useContext(AuthContext);
 
   return (
-    <div>
-      <span>{!ctx.isLogged && "Cabelex"}</span>
-      <span>{ctx.isLogged && "Logo"}</span>
-      <span>{ctx.isLogged && <Link to="/filiais">Filiais</Link>}</span>
-      <span>
-        {ctx.isLogged && <Link to="/funcionarios">Funcionarios</Link>}
-      </span>
-      <span>{ctx.isLogged && <Button>Logout</Button>}</span>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>Cabelex</Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-header" />
+        <Navbar.Collapse id="main-header">
+          <Nav className="me-auto">
+            {ctx.isLogged && (
+              <Link className="nav-link" to="/filiais">
+                Filiais
+              </Link>
+            )}
+            {ctx.isLogged && (
+              <Link className="nav-link" to="/funcionarios">
+                Funcionarios
+              </Link>
+            )}
+          </Nav>
+          {ctx.isLogged && <Button>Logout</Button>}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 

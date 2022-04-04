80504821e0888abb12f5a956db83ae5877fb5b61
Vitor Nascimento <vitor.gan21@gmail.com>
2022-04-03 21:00:01 -0300

    Adicionando componente que fixa o footer na parte inferior da pagina

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -4,8 +4,8 @@ import Container from "react-bootstrap/Container";
 
 const MainFooter = () => {
   return (
-    <Container style={{ marginTop: "auto" }}>
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <Container className="fixed-bottom">
+      <footer className="footer d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top mt-auto">
         <p className="col-md-4 mb-0 text-muted">© 2022 Vitor Nascimento</p>
 
         <div className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none text-muted">

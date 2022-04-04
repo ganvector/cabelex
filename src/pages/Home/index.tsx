f467f74d846ec64ac6787f75203656c64529fd32
Vitor Nascimento <vitor.gan21@gmail.com>
2022-04-03 21:01:27 -0300

    Envolvendo home em um container

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import FiliaisPage from "../FiliaisPage";
 import FuncionariosPage from "../FuncionariosPage";
+import Container from "react-bootstrap/Container";
 
 const Home = () => {
   return (
-    <Routes>
-      <Route path="/" element={<FiliaisPage />} />
-      <Route path="/filiais" element={<FiliaisPage />} />
-      <Route path="/funcionarios" element={<FuncionariosPage />} />
-    </Routes>
+    <Container>
+      <Routes>
+        <Route path="/" element={<FiliaisPage />} />
+        <Route path="/filiais" element={<FiliaisPage />} />
+        <Route path="/funcionarios" element={<FuncionariosPage />} />
+      </Routes>
+    </Container>
   );
 };
 
